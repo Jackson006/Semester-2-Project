a2efae66d8d192e719b456ee8bef7a36ae48947a
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-07-25 12:31:22 +0930

    Update Character.gd
    Created variables for the player's acceleration and maximum speed

--- Project/Characters/Character.gd
+++ Project/Characters/Character.gd
@@ -2,3 +2,6 @@ extends KinematicBody2D
 class_name Character, "res://Art/v1.1 dungeon crawler 16x16 pixel pack/heroes/knight/knight_idle_anim_f0.png" # The character image and class name
 
 const FRICTION: float= 0.15 # Controls the character's friction with the floor
+
+export(int) var acceleration: int = 40
+export(int) var max_speed: int = 100

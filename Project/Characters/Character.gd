c16ef7317ccf086aa3e4859c22029386ecfaefa9
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-07-25 12:50:48 +0930

    FSM script
    Created a script for the Finite State Machine

--- Project/Characters/Character.gd
+++ Project/Characters/Character.gd
@@ -10,6 +10,7 @@ var mov_direction: Vector2 = Vector2.ZERO # A variable that determines the direc
 var velocity: Vector2 = Vector2.ZERO # A variable that determines the velocity/speed of the character
 
 
+# warning-ignore:unused_argument
 func _physics_process(delta: float) -> void: # moves the character using move and lside with velocity and applies friction
 	velocity = move_and_slide(velocity) # Velocity is controlled by the move and slide vectors
 	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
---
+++ Project/Characters/FSM.gd
@@ -0,0 +1,3 @@
+extends Node
+class_name FiniteStateMachine # states the class name as "FiniteStateMachine"
+

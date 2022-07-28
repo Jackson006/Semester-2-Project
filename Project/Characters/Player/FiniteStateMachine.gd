c7b153a556e4e2f6510d7079462baaf7783b8fac
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-07-28 12:37:47 +0930

    Update FiniteStateMachine.gd
    Changed Idle > idle

--- Project/Characters/Player/FiniteStateMachine.gd
+++ Project/Characters/Player/FiniteStateMachine.gd
@@ -5,7 +5,7 @@ func _init() -> void:
 	_add_state("move")
 	
 func _ready() -> void:
-	set_state(states.Idle)
+	set_state(states.idle)
 	
 func _state_logic(_delta: float) -> void:
 	parent.get_input()
@@ -13,17 +13,17 @@ func _state_logic(_delta: float) -> void:
 	
 func _get_transition() -> int:
 	match state:
-		states.Idle:
+		states.idle:
 			if parent.velocity.length() > 10:
 				return states.move
 		states.move:
 			if parent.velocity.length() < 10:
-				return states.Idle
+				return states.idle
 	return -1
 	
 func _enter_state(_previous_state: int, new_state: int) -> void:
 	match new_state:
-		states.Idle:
-			animation_player.play("mdle")
+		states.idle:
+			animation_player.play("idle")
 		states.move:
 			animation_player.play("move")

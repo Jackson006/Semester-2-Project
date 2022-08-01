3e30353b0572949919ac39defa3a15afc28ac9db
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-08-01 21:03:57 +1000

    Update Game.gd
    Created a function that gets and stores the size of the screen and the size of the window and changes the position of the window to half the size of the screen minus half the size of the window

--- Project/Game.gd
+++ Project/Game.gd
@@ -1,16 +1,8 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _init() -> void: # gets and stores the size of the screen and the size of the window
+	var screen_size: Vector2 = OS.get_screen_size()
+	var window_size: Vector2 = OS.get_window_size()
+	# changes the position of the window to half the size of the screen minus half the size of the window
+	OS.set_window_position(screen_size * 0.5 - window_size * 0.5)

7d88d9a694372d0f1fe024607d36c61a16e84d78
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-08-02 09:55:18 +1000

    Update Enemy.gd
    Created chase function

--- Project/Characters/Enemies/Enemy.gd
+++ Project/Characters/Enemies/Enemy.gd
@@ -5,3 +5,8 @@ var path: PoolVector2Array # stores an array of points to the player
 
 onready var navigation: Navigation = get_tree().current_scene.get_node("Navigation")
 onready var player: KinematicBody2D = get_tree().current_scene.get_node("Player")
+
+func chase() -> void:
+	# if the path is not empty, create two variables: vector_to_next_point that stores the vector to the next
+	# point in the path
+	pass

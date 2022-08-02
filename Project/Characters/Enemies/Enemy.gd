df567f1c0269a5e416ae7c3946e8bde4d2dd30be
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-08-02 10:11:50 +1000

    Enemy script
    Added pathfinder code to the enemy script

--- Project/Characters/Enemies/Enemy.gd
+++ Project/Characters/Enemies/Enemy.gd
@@ -9,4 +9,21 @@ onready var player: KinematicBody2D = get_tree().current_scene.get_node("Player"
 func chase() -> void:
 	# if the path is not empty, create two variables: vector_to_next_point that stores the vector to the next
 	# point in the path
-	pass
+	if path:
+		var vector_to_next_point: Vector2 = path[0] - global_position
+		var distance_to_next_point: float = vector_to_next_point.length()
+		# if the distance to the next point is less than 1, remove the first point of the path
+		# and if there aren't more points in the path, return.
+		if distance_to_next_point < 1:
+			path.remove(0)
+			if not path:
+				return
+		# Updates the movement direction with the vector to the next point
+		mov_direction = vector_to_next_point
+		# If the enemy is moving to the right and the animated sprite is flipped, change flip_h to
+		# false to restore the original orientation
+		if vector_to_next_point.x > 0 and animated_sprite.flip_h:
+			animated_sprite.flip_h = false
+		# If the enemy is moving left and the animated sprite is not flipped, flip it
+		elif vector_to_next_point < 0 and not animated_sprite.flip_h:
+			animated_sprite.flip_h = true

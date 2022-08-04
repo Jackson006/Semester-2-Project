8e73d295839b22fb78a7252efec05e68994c88de
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-08-04 12:28:55 +1000

    Update FlyingCreatureFSM.gd
    Added the "chase" state to the script and added the onready function to start the creature on the "chase" state

--- Project/Characters/Enemies/Flying Creature/FlyingCreatureFSM.gd
+++ Project/Characters/Enemies/Flying Creature/FlyingCreatureFSM.gd
@@ -1,2 +1,7 @@
 extends FiniteStateMachine
 
+func _init() -> void:
+	_add_state("chase") # adds the chase state to the script
+
+func _ready() -> void:
+	set_state(states.chase) # sets the starting state to the chase state

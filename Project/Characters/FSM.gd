6e3c4fb3833425f8af100941c1a65b0dbf9c0e81
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-08-04 12:30:06 +1000

    Update FSM.gd

--- Project/Characters/FSM.gd
+++ Project/Characters/FSM.gd
@@ -10,7 +10,6 @@ var state: int = -1 setget set_state # The variable state stores the current sta
 onready var parent: Character = get_parent()
 onready var animation_player: AnimationPlayer = parent.get_node("AnimationPlayer")
 
-
 func _physics_process(delta: float) -> void:
 	if state != -1: # checks if the state is not null
 		_state_logic(delta) # calls the function _state_logic
@@ -21,26 +20,21 @@ func _physics_process(delta: float) -> void:
 
 func _state_logic(_delta: float) -> void:
 	pass
-	
-	
+
 func _get_transition() -> int:
 	return -1
 
-
 func _add_state(new_state: String) -> void:
 	states[new_state] = states.size()
-	
-	
+
 func set_state(new_state: int) -> void: # Calls the function _exit_state, updates the previous_state, states varables, anf calls the function _enter_state
 	_exit_state(state)
 	previous_state = state
 	state = new_state
 	_enter_state(previous_state, state)
 
-
 func _enter_state(_previous_state: int, _new_state: int) -> void:
 	pass
-	
-	
+
 func _exit_state(_state_exited: int) -> void:
 	pass

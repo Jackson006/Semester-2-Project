3b78355c7099d6f3650a1d52dd18960b73d14e18
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-08-09 10:52:32 +1000

    Update Hitbox.gd
    Added comments to the hitbox script

--- Project/Hitbox.gd
+++ Project/Hitbox.gd
@@ -1,17 +1,17 @@
 extends Area2D
 class_name Hitbox
 
-export(int) var damage: int = 1
-var knockback_direction: Vector2 = Vector2.ZERO
-export(int) var knockback_force: int = 300
+export(int) var damage: int = 1 # damage variable
+var knockback_direction: Vector2 = Vector2.ZERO # Direction of impact variable
+export(int) var knockback_force: int = 300 # Knockback force variable
 
-onready var collision_shape: CollisionShape2D = get_child(0)
+onready var collision_shape: CollisionShape2D = get_child(0) # collision shape to be added as a child node
 
 func _init() -> void:
-	connect("body_entered", self, "_on_body_entered")
+	connect("body_entered", self, "_on_body_entered") # Connects the signal to the entered body
 
 func _ready() -> void:
-	assert(collision_shape != null)
+	assert(collision_shape != null) # Makes sure the hitbox has a collision shape
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
-	body.take_damage(damage, knockback_direction, knockback_force)
+	body.take_damage(damage, knockback_direction, knockback_force) # When a body enters the collision area, call the function take_damage of the body with the variables of the script as parameters

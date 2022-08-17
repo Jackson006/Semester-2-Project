d6b43ee10a7c9aa264cc28f806dfd5edfde07915
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-08-17 16:51:06 +1000

    _on_spawn_explosion_animation_finished
    Que frees the animation when it finishes playing

--- Project/Characters/Enemies/SpawnExplosion.gd
+++ Project/Characters/Enemies/SpawnExplosion.gd
@@ -2,3 +2,7 @@ extends AnimatedSprite
 
 func _ready() -> void:
 	playing = true
+
+func _on_SpawnExplosion_animation_finished() -> void:
+	# When the animation stops playing, free the scene
+	queue_free()

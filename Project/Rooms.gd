89858401be43b9452e20c2f144ccd8a8e18891f7
Jackson006 <0337377@schoolsnet.act.edu.au>
2022-08-30 10:22:54 +1000

    lot of big boy code
    added more lines of code to the room script

--- Project/Rooms.gd
+++ Project/Rooms.gd
@@ -1,10 +1,31 @@
 extends Navigation2D
 
 const SPAWN_ROOMS: Array = [preload("res://Rooms/SpawnRoom0.tscn"), preload("res://Rooms/SpawnRoom1.tscn")]
-const INTERMEDIATE_ROOMS: Array = [preload("res://Rooms/Room0.tscn"), preload(""), preload("")]
+const INTERMEDIATE_ROOMS: Array = [preload("res://Rooms/Room0.tscn"), preload("res://Rooms/Room1.tscn"), preload("res://Rooms/Room2.tscn")]
 const END_ROOMS: Array = [preload("res://Rooms/EndRoom0.tscn")]
 
 const TILE_SIZE: int = 16
 const FLOOR_TILE_INDEX: int = 22
 const RIGHT_WALL_TILE_INDEX: int = 6
 const LEFT_WALL_TILE_INDEX: int = 7
+
+export(int) var num_levels: = 5
+
+onready var player: KinematicBody2D = get_parent().get_node("Player")
+
+
+func _ready() -> void:
+	_spawn_rooms()
+	
+
+func _spawn_rooms() -> void:
+	var previous_room: Node2D
+	
+	for i in num_levels:
+		var room: Node2D
+		
+		if i == 0:
+			room = SPAWN_ROOMS[randi() % SPAWN_ROOMS.size()].instance()
+			player.positian = room.get_node("PlayerSpawnPos").position
+		add_child(room)
+		previous_room = room

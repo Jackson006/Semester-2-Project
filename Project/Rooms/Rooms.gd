d637b6c6ff421b23254bb838313f05703a10e7f6
Jackson006 <0337377@schoolsnet.act.edu.au>
2022-09-01 12:47:11 +1000

    updates

--- Project/Rooms/Rooms.gd
+++ Project/Rooms/Rooms.gd
@@ -31,7 +31,7 @@ func _spawn_rooms() -> void:
 			if i == num_levels -1:
 				room = END_ROOMS[randi() % END_ROOMS.size()]
 			else:
-				room = INTERMEDIATE_ROOMS[randi() % INTERMEDIATE_ROOMS.size()]
+				room = INTERMEDIATE_ROOMS[randi() % INTERMEDIATE_ROOMS.size()] 
 				
 			var previous_room_tilemap: TileMap = previous_room.get_node("TileMap")
 			var previous_room_door: StaticBody2D = previous_room.get_node("Doors/Door")
@@ -43,6 +43,9 @@ func _spawn_rooms() -> void:
 				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(-1, -y), FLOOR_TILE_INDEX)
 				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(-0, -y), FLOOR_TILE_INDEX)
 				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(-2, -y), RIGHT_WALL_TILE_INDEX)
+				
+			var room_tilemap: TileMap = room.et_node("TileMap")
+			room.position = previous_room_door.global_position + Vector2.UP * room_tilemap.get_used_rect().size.y * TILE_SIZE + Vector2.UP * (1 + corridor_height) * TILE_SIZE + Vector2.LEFT * room_tilemap.world_to_map(room.get_node("Entrance/Position2D2").position).x * TILE_SIZE
 				
 		add_child(room)
 		previous_room = room

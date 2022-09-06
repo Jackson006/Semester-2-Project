ca7818053c86066771bb5ad97a6428697badbef0
Jackson006 <0337377@schoolsnet.act.edu.au>
2022-09-06 10:25:35 +1000

    added to network setup

--- Project/Networking/Network_setup.gd
+++ Project/Networking/Network_setup.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var player = load("res://Player.tscn")
+
 onready var multiplayer_config_ui = $Multiplayer_configure
 onready var server_ip_address = $Multiplayer_configure/Server_ip_address
 
@@ -9,21 +11,38 @@ func _ready() -> void:
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
 	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
 	
 	device_ip_address.text = Network.ip_address
 
 func _player_connected(id) -> void:
 	print("player " + str(id) + "has connected")
+	
+	instance_player(id)
 
 func _player_disconnected(id) -> void:
 	print("player " + str(id) + "has disconnected")
+	
+	if Players.has_node(str(id)):
+		Players.get_node(str(id)).queue_free()
 
 func _on_Create_server_pressed():
 	multiplayer_config_ui.hide()
 	Network.create_server()
+	
+	instance_player(get_tree().get_network_unique_id())
 
 func _on_Join_server_pressed():
 	if server_ip_address.text != "":
 		multiplayer_config_ui.hide()
 		Network.ip_address = server_ip_address.text
 		Network.join_server()
+
+func _connected_to_server() -> void:
+	yield(get_tree().create_timer(0.1), "timeout")
+	instance_player(get_tree().get_network_unique_id())
+
+func instance_player(id) -> void:
+	var player_instance = Globle.instance_node_at_location(player, Players, Vector2(160, 176))
+	player_instance.name = str(id)
+	player_instance.set_network_master(id)

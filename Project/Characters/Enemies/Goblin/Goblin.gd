58c2529eadef9f4720be4e0c69fa11d7ec0e35d8
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-09-20 10:51:33 +1000

    Finished goblin code

--- Project/Characters/Enemies/Goblin/Goblin.gd
+++ Project/Characters/Enemies/Goblin/Goblin.gd
@@ -1,10 +1,17 @@
 extends Enemy
 
+const THROWABLE_KNIFE_SCENE: PackedScene = preload("res://Characters/Enemies/Goblin/ThrowableKnife.tscn")
 const MAX_DISTANCE_TO_PLAYER: int = 80
 const MIN_DISTANCE_TO_PLAYER: int = 40
 
+export(int) var projectile_speed: int = 15
+
+var can_attack: bool = true
+
 # variable for the goblin's distance from the player
 var distance_to_player: float 
+
+onready var attack_timer: Timer = get_node("AttackTimer")
 
 # updates the distance to the player 
 func _on_PathTimer_timeout() -> void:
@@ -16,6 +23,11 @@ func _on_PathTimer_timeout() -> void:
 		# distance constant, get the path to move away from the player
 		elif distance_to_player < MIN_DISTANCE_TO_PLAYER:
 			_get_path_to_move_away_from_player()
+		else:
+			if can_attack:
+				can_attack = false
+				_throw_knife()
+				attack_timer.start()
 	# If the player is not a valid instance, stop the timer, empty the path 
 	# array, and change the movement direction to an empty vector
 	else:
@@ -28,4 +40,10 @@ func _get_path_to_move_away_from_player() -> void:
 	var dir: Vector2 = (global_position - player.position).normalized()
 	path = navigation.get_simple_path(global_position, global_position + dir * 100)
 
+func _throw_knife() -> void:
+	var projectile: Area2D = THROWABLE_KNIFE_SCENE.instance()
+	projectile.launch(global_position, (player.position - global_position).normalized(), projectile_speed)
+	get_tree().current_scene.add_child(projectile)
 
+func _on_AttackTimer_timeout() -> void:
+	can_attack = true

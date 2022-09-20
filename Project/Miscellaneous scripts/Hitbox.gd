082ab1d124cfa0c7d3a11ff9103952adef4932fe
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-09-20 11:33:46 +1000

    fixed hitbox damage bug
    Enemies can now deal damage to players without needing the player to exit the hitbox

--- Project/Miscellaneous scripts/Hitbox.gd
+++ Project/Miscellaneous scripts/Hitbox.gd
@@ -5,16 +5,30 @@ export(int) var damage: int = 1 # damage variable
 var knockback_direction: Vector2 = Vector2.ZERO # Direction of impact variable
 export(int) var knockback_force: int = 300 # Knockback force variable
 
+var body_inside: bool = false
+
 onready var collision_shape: CollisionShape2D = get_child(0) # collision shape to be added as a child node
+onready var timer: Timer = Timer.new()
 
 func _init() -> void:
-	connect("body_entered", self, "_on_body_entered") # Connects the signal to the entered body
+	var __ = connect("body_entered", self, "_on_body_entered") # Connects the signal to the entered body
+	__ = connect("body_exited", self, "_on_body_exited")
 
 func _ready() -> void:
 	assert(collision_shape != null) # Makes sure the hitbox has a collision shape
+	timer.wait_time = 1
+	add_child(timer)
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
-	_collide(body)
+	body_inside = true
+	timer.start()
+	while body_inside:
+		_collide(body)
+		yield(timer, "timeout")
+
+func _on_body_exited(_body: KinematicBody2D) -> void:
+	body_inside = false
+	timer.stop()
 
 func _collide(body: KinematicBody2D) -> void:
 	# Checks if the body parameter is null 

4bda9e8a68a48667d1b570895bf76c12409d87af
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-10-12 22:05:28 +1100

    Finished stairs script

--- Project/Rooms/Rooms, Furniture and Traps/Stairs.gd
+++ Project/Rooms/Rooms, Furniture and Traps/Stairs.gd
@@ -1,2 +1,10 @@
 extends Area2D
 
+# Stores the collision shape in an onready variable
+onready var collision_shape: CollisionShape2D = get_node("CollisionShape2D")
+
+func _on_Stairs_body_entered(_body: KinematicBody2D) -> void:
+# Disables the collision when the player enters the stairs 
+	collision_shape.set_deferred("disabled", true)
+	# Starts transition to the game's scene
+	SceneTransistor.start_transistion_to("res://Game.tscn")

edc7e13dc7661638a383b1c6f28d2d18252a2f60
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-10-25 10:27:31 +1100

    ???

--- Project/Rooms/Room.gd
+++ Project/Rooms/Room.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+export(bool) var boss_room: bool = false
+
 # Variable for the explosion animation
 const SPAWN_EXPLOSION_SCENE: PackedScene = preload("res://Characters/Enemies/SpawnExplosion.tscn")
 # Stores enemy scenes
-const ENEMY_SCENES: Dictionary = {"FLYING_CREATURE": preload("res://Characters/Enemies/Flying Creature/FlyingCreature.tscn"), "GOBLIN": preload("res://Characters/Enemies/Goblin/Goblin.tscn")}
+const ENEMY_SCENES: Dictionary = {"FLYING_CREATURE": preload("res://Characters/Enemies/Flying Creature/FlyingCreature.tscn"), "GOBLIN": preload("res://Characters/Enemies/Goblin/Goblin.tscn"), "SLIME_BOSS": preload("res://Characters/Enemies/Bosses/SlimeBoss.tscn")}
 
 var num_enemies: int
 
@@ -39,11 +41,14 @@ func _spawn_enemies() -> void:
 	# Spawns the enemies and the explosion effect. For each position spawn the enemy and spawn an explosion
 	for enemy_position in enemy_positions_container.get_children():
 		var enemy: KinematicBody2D
-		if randi() %2 == 0:
-			enemy = ENEMY_SCENES.FLYING_CREATURE.instance()
-		else:
-			enemy = ENEMY_SCENES.GOBLIN.instance()
-		var __ = enemy.connect("tree_exited", self, "_on_enemy_killed")
+		if boss_room:
+			enemy = ENEMY_SCENES.SLIME_BOSS.instance()
+			num_enemies = 15
+		else: 
+			if randi() %2 == 0:
+				enemy = ENEMY_SCENES.FLYING_CREATURE.instance()
+			else:
+				enemy = ENEMY_SCENES.GOBLIN.instance()
 		enemy.position = enemy_position.position
 		call_deferred("add_child", enemy)
 		

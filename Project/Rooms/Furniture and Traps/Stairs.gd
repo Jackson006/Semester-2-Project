6b918ae30c3a4a7bafc9e480674aa32157490c7b
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2022-11-08 10:56:35 +1100

    Fixed heal potion bug
    Fixed bug where health gained from health potions was not saved between  scenes

--- Project/Rooms/Furniture and Traps/Stairs.gd
+++ Project/Rooms/Furniture and Traps/Stairs.gd
@@ -4,4 +4,4 @@ onready var collision_shape: CollisionShape2D = get_node("CollisionShape2D")
 
 func _on_Stairs_body_entered(_body: KinematicBody2D) -> void:
 	SceneTransistor.start_transition_to("res://Game.tscn")
-	# get_tree().change_scene("res://Game.tscn")
+
